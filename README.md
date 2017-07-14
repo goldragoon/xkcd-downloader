3e6388b2ef0176a86ac26fc80049756334091ca4
Neel Kamath <neelkamath@protonmail.com>
2017-07-14 18:28:24 +0530

    Allowed the automatic downloading of comics

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # xkcd Downloader
 
-Ever been bored and wanted offline access to [xkcd](https://xkcd.com/)? This software downloads each comic from xkcd skipping those already downloaded without the need for SQL, complex command line arguments etc. To facilitate easier reading, comics are stored in year->month subdirectories.
+Ever been bored and wanted offline access to [xkcd](https://xkcd.com/)? This software downloads each comic from xkcd skipping those already downloaded without the need for SQL, complex command line arguments etc. To facilitate easier reading, comics are stored in `year/month` subdirectories. A few comic numbers might be skipped because they either don't exist or don't contain a visual.
 
 # Installation
 
@@ -16,9 +16,15 @@ Ever been bored and wanted offline access to [xkcd](https://xkcd.com/)? This sof
 
 # Usage
 
-1. Run the script:
+1. Change the directory: `cd src`
+1. Run:
     - Windows: `python downloader.py`
     - Other: `python3 downloader.py`
+1. If this is the first time running it, it will immediately terminate after generating the file `config.json`. If you want to change the directory it downloads comics to (by default, it downloads to `comics` located in the scripts' directory), you may do so in `config.json`. Run the script again if necessary.
+1. It's also possible to have the script run each time your computer powers on so that new comics are automatically downloaded. The steps to do so are listed below but only work on Windows as other distros have varying steps to achieve this:
+    1. Go to the directory `C:\Users\<YOUR_USERNAME>\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup`, where `<YOUR_USERNAME>` is your username.
+    1. Create a shortcut pointing to the location of the script.
+    1. Make sure python scripts open with `Python` (so that when you open the file from File Explorer it runs the script and doesn't open the editor). You can do this by right-clicking the script, clicking `Opens with:` in the `General` tab and choosing `Python`.
 
 # Contributing
 
--- src/downloader.py
+++ src/downloader.py
@@ -5,24 +5,26 @@ import os
 import os.path
 import re
 import requests
+import sys
 import urllib.request
 
-
 if __name__ == "__main__":
-    directory = input(
-        "Enter the path to save the comics to (if you don't enter a path, "
-        + "they will be saved to a directory named \"comics\" in the "
-        + "directory of this program):")
-    print("\nPlease note that certain comics have been skipped as they either "
-          + "don't exist or don't have a visual.\n")
-    if not directory:
-        directory = "comics"
+    config = "config.json"
+    if not os.path.isfile(config):
+        with open(config, "w") as f:
+            data = {"directory": "{}/comics".format(os.getcwd())}
+            json.dump(data, f, indent = 4)
+            sys.exit()
+    data = json.loads(open(config).read())
+    directory = data["directory"]
     if not os.path.isdir(directory):
         os.makedirs(directory)
     dataFile = "{}/DO_NOT_DELETE.json".format(directory)
+    # <"last"> in the JSON file corresponds to the last comic downloaded.
     if not os.path.isfile(dataFile):
         with open(dataFile, "w") as f:
             json.dump({"last": 0}, f, indent = 4)
+    # http://xkcd.com/info.0.json is the link to the latest comic.
     data = requests.get("http://xkcd.com/info.0.json").json()
     numOfComics = data["num"]
     f = json.loads(open(dataFile).read())
@@ -32,24 +34,27 @@ if __name__ == "__main__":
             percentage = 100
         else:
             percentage = ((comic - start) / (numOfComics - start)) * 100
+        # An extra space is kept after the percentage as <end = "\r"> doesn't
+        # rewrite the entire line so 34.5% becomes 34.5<previous second digit>.
         print("Progress: {}% ".format(round(percentage, 2)), end = "\r")
         site = "http://xkcd.com/{}/info.0.json".format(comic)
         data = requests.get(site)
         if data.status_code != 200:
             continue
         data = data.json()
-        if not re.search(r".png$", data["img"]):
+        if not re.search(r".jpg|.png$", data["img"]):
             continue
         months = {1: "January", 2: "February", 3: "March", 4: "April",
                   5: "May", 6: "June", 7: "July", 8: "August", 9: "September",
                   10: "October", 11: "November", 12: "December"}
         m = months[int(data["month"])]
+        m = "{}-{}".format(data["month"], m)
         newDir = "{}/{}/{}".format(directory, data["year"], m)
         if not os.path.isdir(newDir):
             os.makedirs(newDir)
         title = re.sub(r"/|\\|\:|\*|\?|\"|<|>|\|", "", data["safe_title"])
-        name = "{}/{}-{}.png".format(newDir, comic, title)
+        ext = data["img"][len(data["img"]) - 4:]
+        name = "{}/{}-{}{}".format(newDir, comic, title, ext)
         urllib.request.urlretrieve(data["img"], name)
         with open(dataFile, "w") as f:
             json.dump({"last": comic}, f, indent = 4)
-    print("\nCompleted")
